3922f73ff5d99ee149de6b9bb107859ad072c08a
Muhamad Rizki <muhamadrizkiy@gmail.com>
2020-06-26 01:43:13 +0700

    authorization

--- common/models/wallet.js
+++ common/models/wallet.js
@@ -3,13 +3,12 @@
 let crypto = require('crypto')
 
 module.exports = function(Wallet) {
-
-
+  
     let MSG_ERR_NOT_FOUND = 'Cannot Find The Wallet. Please register your wallet!'
-    let MSG_ERR_EXIST = 'The Wallet Already Exists. Try to Deposits'
-    let MSG_ERR_BALANCE = 'The amount being used must not be more than the current balance'
-    let MSG_ERR_DISABLE = 'Please Enable Your Wallet'
-
+    let MSG_ERR_EXIST = 'The Wallet Already Exists. Try to Deposits!'
+    let MSG_ERR_BALANCE = 'The amount being used must not be more than the current balance.'
+    let MSG_ERR_DISABLE = 'Please Enable Your Wallet!'
+    let MSG_ERR_LOGIN = 'Authorization Required!'
 
     Wallet.disableRemoteMethodByName('create'); 
     Wallet.disableRemoteMethodByName('upsert');
@@ -27,56 +26,65 @@ module.exports = function(Wallet) {
     Wallet.disableRemoteMethodByName('replace');
 
     Wallet.enableWallet = function(options, callback) {
-
+      
       const walletId = crypto.randomBytes(16).toString("hex");
+     
+      if (!options.accessToken) {
+        let data = {
+          status : 'Failed',
+          message : MSG_ERR_LOGIN
+        }
+        callback (null, data);
+      } else {
+        let userId = options.accessToken.userId
 
-      let data = {
-        status: "success",
-        data: {
-          wallet: {
-            id: walletId,
-            owned_by: options.accessToken.userId,
-            status: "enabled",
-            enabled_at: Date.now(),
-            balance: 0
-          }
-        } 
-      }
-
-      var filter = {
-        where: {
+        var data = {
+          status: "success",
           data: {
-            owned_by : options.accessToken.userId
+            wallet: {
+              id: walletId,
+              owned_by: userId,
+              status: "enabled",
+              enabled_at: Date.now(),
+              balance: 0
+            }
+          } 
+        }
+
+        var filter = {
+          where: {
+            data: {
+              owned_by : userId
+            }
           }
         }
+
+        Wallet.findOne(filter).then((checkState) => {
+          if(!checkState) {
+            data.is_disable = false
+            Wallet.create(data, function(err, res){
+              if(err) {
+                callback(err);
+              }
+              else {
+                callback(null, res);
+              }
+            });
+          }
+          else if (checkState && checkState.is_disable == true) {
+            checkState.updateAttributes({is_disable: false}, function(err, res){
+              callback (null, res);
+            })
+          } 
+          else {
+            let data = {
+              status : 'Failed',
+              message : MSG_ERR_EXIST
+            }
+            callback (null, data);
+          }
+        })
       }
-
-      Wallet.findOne(filter).then((checkState) => {
-        if(!checkState) {
-          data.is_disable = false
-          Wallet.create(data, function(err, res){
-            if(err) {
-              callback(err);
-            }
-            else {
-              callback(null, res);
-            }
-          });
-        }
-        else if (checkState && checkState.is_disable == true) {
-          checkState.updateAttributes({is_disable: false}, function(err, res){
-            callback (null, res);
-          })
-        } 
-        else {
-          let data = {
-            status : 'Failed',
-            message : MSG_ERR_EXIST
-          }
-          callback (null, data);
-        }
-      })
-
     };
     
     Wallet.remoteMethod(
@@ -93,29 +101,35 @@ module.exports = function(Wallet) {
         }
       );
 
-
     Wallet.getBalance = function(options,callback) {
-
-      var filter = {
-        where: {
-          data: {
-            owned_by : options.accessToken.userId
+      
+      if (!options.accessToken) {
+        let data = {
+          status : 'Failed',
+          message : MSG_ERR_LOGIN
+        }
+        callback (null, data);
+      } else {
+        let userId = options.accessToken.userId
+        var filter = {
+          where: {
+            data: {
+              owned_by : userId
+            }
           }
         }
+        Wallet.findOne(filter,function(err, result){
+          if(err) callback(err);
+          else if (!result || result.is_disable){
+            let data = {
+              status : 'Failed',
+              message : MSG_ERR_DISABLE
+            }
+            callback (null, data);
+          }
+          else callback (null, result);
+        });
       }
-
-      Wallet.findOne(filter,function(err, result){
-        if(err) callback(err);
-        else if (!result || result.is_disable){
-          let data = {
-            status : 'Failed',
-            message : MSG_ERR_DISABLE
-          }
-          callback (null, data);
-        }
-        else callback (null, result);
-      });
-
     };
 
     Wallet.remoteMethod(
@@ -132,38 +146,43 @@ module.exports = function(Wallet) {
       }
     );
 
-
     Wallet.deposit = function(amounts, options, callback) {
-
-      const reference_id = crypto.randomBytes(16).toString("hex");
-
-      let userId = options.accessToken.userId
-
-      var filter = {
-        where: {
-          data: {
-            owned_by : userId
+      
+      if (!options.accessToken) {
+        let data = {
+          status : 'Failed',
+          message : MSG_ERR_LOGIN
+        }
+        callback (null, data);
+      } else {
+        let userId = options.accessToken.userId
+        var filter = {
+          where: {
+            data: {
+              owned_by : userId
+            }
           }
         }
+
+        const reference_id = crypto.randomBytes(16).toString("hex");
+
+        Wallet.findOne(filter).then((updateState) => {
+          if(!updateState) callback(err);
+          else if (updateState.is_disable){
+            let data = {
+              status : 'Failed',
+              message : MSG_ERR_DISABLE
+            }
+            callback (null, data);
+          }
+          else {
+            let newAmount = updateState.amount + amounts
+            updateState.updateAttributes({amount: newAmount}, function(err, res){
+              callback (null, res);
+            })
+          } 
+        })
       }
-
-      Wallet.findOne(filter).then((updateState) => {
-        if(!updateState) callback(err);
-        else if (updateState.is_disable){
-          let data = {
-            status : 'Failed',
-            message : MSG_ERR_DISABLE
-          }
-          callback (null, data);
-        }
-        else {
-          let newAmount = updateState.amount + amounts
-          updateState.updateAttributes({amount: newAmount}, function(err, res){
-            callback (null, res);
-          })
-        } 
-      })
-
     }
 
     Wallet.remoteMethod(
@@ -183,42 +202,49 @@ module.exports = function(Wallet) {
 
     Wallet.withdrawals = function(amounts, options, callback) {
 
-      const reference_id = crypto.randomBytes(16).toString("hex");
-
-      let userId = options.accessToken.userId
-
-      var filter = {
-        where: {
-          data: {
-            owned_by : userId
+      if (!options.accessToken) {
+        let data = {
+          status : 'Failed',
+          message : MSG_ERR_LOGIN
+        }
+        callback (null, data);
+      } else {
+        let userId = options.accessToken.userId
+        var filter = {
+          where: {
+            data: {
+              owned_by : userId
+            }
           }
         }
+        const reference_id = crypto.randomBytes(16).toString("hex");
+
+        Wallet.findOne(filter).then((updateState) => {
+          if(!updateState) callback(err);
+          else if (updateState.is_disable){
+            let data = {
+              status : 'Failed',
+              message : MSG_ERR_DISABLE
+            }
+            callback (null, data);
+          }
+          else if (amounts > updateState.amount) {
+            let data = {
+              status : 'Failed',
+              message : MSG_ERR_BALANCE
+            }
+            callback (null, data);
+          }
+          else {
+            let newAmount = updateState.amount - amounts
+            updateState.updateAttributes({amount: newAmount}, function(err, res){
+              if (err) callback(err)
+              else callback (null, res);
+            })
+          } 
+        })
       }
-
-      Wallet.findOne(filter).then((updateState) => {
-        if(!updateState) callback(err);
-        else if (updateState.is_disable){
-          let data = {
-            status : 'Failed',
-            message : MSG_ERR_DISABLE
-          }
-          callback (null, data);
-        }
-        else if (amounts > updateState.amount) {
-          let data = {
-            status : 'Failed',
-            message : MSG_ERR_BALANCE
-          }
-          callback (null, data);
-        }
-        else {
-          let newAmount = updateState.amount - amounts
-          updateState.updateAttributes({amount: newAmount}, function(err, res){
-            callback (null, res);
-          })
-        } 
-      })
-
+      
     }
 
     Wallet.remoteMethod(
@@ -236,32 +262,40 @@ module.exports = function(Wallet) {
       }
     );
 
-
     Wallet.disableWallet = function(options, callback) {
+      
 
-      var filter = {
-        where: {
-          data: {
-            owned_by : options.accessToken.userId
+      if (!options.accessToken) {
+        let data = {
+          status : 'Failed',
+          message : MSG_ERR_LOGIN
+        }
+        callback (null, data);
+      } else {
+        let userId = options.accessToken.userId
+        var filter = {
+          where: {
+            data: {
+              owned_by : userId
+            }
           }
         }
+
+        Wallet.findOne(filter).then((checkState) => {
+          if(!checkState) {
+            let data = {
+              status : 'Failed',
+              message : MSG_ERR_NOT_FOUND
+            }
+            callback (null, data);
+          }
+          else {
+            checkState.updateAttributes({is_disable: true}, function(err, res){
+              callback (null, res);
+            })
+          }
+        })
       }
-
-      Wallet.findOne(filter).then((checkState) => {
-        if(!checkState) {
-          let data = {
-            status : 'Failed',
-            message : MSG_ERR_NOT_FOUND
-          }
-          callback (null, data);
-        }
-        else {
-          checkState.updateAttributes({is_disable: true}, function(err, res){
-            callback (null, res);
-          })
-        }
-      })
-
     };
     
     Wallet.remoteMethod(
