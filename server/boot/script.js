0577f40771cba81d8d80615c5e5ba67c129d0b30
Muhamad Rizki <muhamadrizkiy@gmail.com>
2020-06-28 23:49:52 +0700

    missed up to push

--- server/boot/script.js
+++ server/boot/script.js
@@ -1,7 +1,11 @@
 var loopback = require('loopback');
 
+module.exports = function(app) {
+
 var memory = loopback.createDataSource({
     connector: loopback.Memory,
     file: "mydata.json"
 });
+
+};
 
